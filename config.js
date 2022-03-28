77151127704e847c4cf88f7bbef9bbfc21dee1f3
ning <2320396478@qq.com>
2022-03-28 17:14:09 +0800

    init

---
+++ config.js
@@ -0,0 +1,9 @@
+(function (global) {
+    global.__config__ = {
+        // API_PATH:'/portal',    // 线上请求地址(直连)
+        API_PATH:'/portal/compliance',    // 线上请求地址(转)
+        // API_PATH:'/api',    // 请求地址
+        // API_PATH:'/test',    // 本地请求地址
+        // PORTAL_PATH:'/portal',    // 本地请求地址
+    }
+})(window)
